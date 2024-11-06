5286279265d01602eff8636823cfef71cec77f57
arielGs_Dev <arielogavilanes1534@gmail.com>
2024-11-05 21:44:43 -0500

    first commit

---
+++ src/main/java/com/yavirac/api_tareas_crud/ApiTareasCrudApplication.java
@@ -0,0 +1,13 @@
+package com.yavirac.api_tareas_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTareasCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTareasCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yavirac/api_tareas_crud/ApiTareasCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yavirac.api_tareas_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTareasCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
