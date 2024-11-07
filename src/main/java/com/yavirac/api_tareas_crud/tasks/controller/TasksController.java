d587e31fe700d227120c1fb86d7adc7c542e2785
users <jam.altamirano@yavirac.edu.ec>
2024-11-06 22:13:45 -0500

    Implentacion de traer todas las tareas con el getAll

--- src/main/java/com/yavirac/api_tareas_crud/tasks/controller/TasksController.java
+++ src/main/java/com/yavirac/api_tareas_crud/tasks/controller/TasksController.java
@@ -1,10 +1,24 @@
 package com.yavirac.api_tareas_crud.tasks.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.yavirac.api_tareas_crud.tasks.model.Tasks;
+import com.yavirac.api_tareas_crud.tasks.service.TasksService;
 
 @RestController
 @RequestMapping("/api/tasks")
 public class TasksController {
+    @Autowired
+    private TasksService tasksService;
+
+    @GetMapping("/all")
+    public List<Tasks> getAllTasks() {
+        return tasksService.getAll();
+    }
 
 }
--- src/main/java/com/yavirac/api_tareas_crud/tasks/service/TasksService.java
+++ src/main/java/com/yavirac/api_tareas_crud/tasks/service/TasksService.java
@@ -1,9 +1,21 @@
 package com.yavirac.api_tareas_crud.tasks.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.yavirac.api_tareas_crud.tasks.model.Tasks;
+import com.yavirac.api_tareas_crud.tasks.repository.TasksRepository;
 
 
 @Service
 public class TasksService {
+    @Autowired
+    private TasksRepository tasksRepository;
+
+    public List<Tasks> getAll() {
+        return tasksRepository.findAll();
+    }
 
 }
